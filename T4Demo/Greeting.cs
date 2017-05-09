20589b02596d8eda21f1a33300933e974b3e100a
Woody Lee <woody.lee@EF.com>
2017-05-09 11:50:20 +0800

    finish the hello world sample

---
+++ T4Demo/Greeting.cs
@@ -0,0 +1,22 @@
+﻿ 
+
+// Generated from template
+// DO NOT change manually
+
+using System;
+using System.Collections.Generic;
+
+namespace T4Ddemo.Entity
+{
+	class Greeting
+	{
+		public static Dictionary<String, String> GetGlobalGreetings()
+        {
+            var result = new Dictionary<String, String>();			
+            result.Add("en-US", "Good morning!");
+            result.Add("zh-CN", "早上好！");
+            return result;
+        }
+	}
+}
+
---
+++ T4Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using T4Ddemo.Entity;
+
+namespace T4Demo
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            var dict = Greeting.GetGlobalGreetings();
+            foreach(var item in dict)
+            {
+                Console.WriteLine(item.Key + ", " + item.Value);                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
